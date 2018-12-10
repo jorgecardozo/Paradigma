841b139d0ce6ffc61c9d2646a0a235f070680feb
Jorge Cardozo <jorge.cardozo@paradigma.pds>
2018-12-10 17:27:52 -0300

    Se agrego el POST - DELETE

--- prueba/src/Componentes/Main.js
+++ prueba/src/Componentes/Main.js
@@ -1,126 +1,152 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import axios from 'axios'
 import Tablas from './Tablas';
 
-class Main extends Component{
+class Main extends Component {
 
-    
-    constructor () {
+
+    constructor() {
         super();
         this.state = {
-          nombre: '',
-          apellido: '',
-          tipo: 'DNI',
-          documento: '',
-          email: '',
-          datos:[]
+            nombre: '',
+            apellido: '',
+            tipo: 1,
+            documento: '',
+            email: '',
+            datos: []
         }
-
-        axios.get('http://localhost:3001/personas')
-        .then(function (response) {
-            console.log("Las pido");
-            console.log(response.data);
-
-            this.setState({datos : response.data});
-            console.log("veo si actualizo en datos");
-            console.log(this.state.datos);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-
-       
     }
 
-    /*componentDidMount(){
-        axios.get('http://localhost:3001/personas')
-              .then(function (response) {
-                console.log(response);
-                this.setState({datos:response.data});
-              })
-              .catch(function (error) {
+    componentDidMount(){
+             this.actualizar();
+    }
+
+    actualizar =(e)=>{
+
+        console.log("LLamo a la funcion Actualizar");
+      
+        axios.get('http://10.0.0.68:81/personas/')
+            .then((response)=> {
+                console.log("Las pido");
+                console.log(response.data.data);
+
+                this.setState({ datos: response.data.data }, () => {
+                    console.log("veo si actualizo en datos");
+                    console.log(this.state.datos);
+                });
+            })
+            .catch(function (error) {
                 console.log(error);
-              });
-    }
-    */
+            });
 
-    validarNumero = (e) =>{
-        const {value, name} = e.target;
-        var expresionRegular=/^\d{0,8}$/;
-        if(expresionRegular.test(value)){
-        console.log(value,name);
-        // console.log("Error");
+
+    }
+    
+
+    validarNumero = (e) => {
+        const { value, name } = e.target;
+        var expresionRegular = /^\d{0,8}$/;
+        if (expresionRegular.test(value)) {
+            console.log(value, name);
+            // console.log("Error");
             this.setState({
                 [name]: value
-              });
+            });
         }
-       
+
     }
 
-    validarMail = (e) =>{
-        const {value, name} = e.target;
-        
-            this.setState({
-                [name]: value
-              }); 
+    validarMail = (e) => {
+        const { value, name } = e.target;
+
+        this.setState({
+            [name]: value
+        });
     }
 
-    handleSubmit = (e) =>{
+    handleSubmit = (e) => {
         e.preventDefault();
 
-        const {value, name} = e.target;
-        var expresionRegular=/^[a-zA-Z0-9_\-\.~]{2,}@[a-zA-Z0-9_\-\.~]{2,}\.[a-zA-Z]{2,4}$/;
-        console.log(value,name);
-        if(expresionRegular.test(this.state.email)){
+        console.log("Valor del Boton");
+        console.log(e.value);
+
+        const { value, name } = e.target;
+        var expresionRegular = /^[a-zA-Z0-9_\-\.~]{2,}@[a-zA-Z0-9_\-\.~]{2,}\.[a-zA-Z]{2,4}$/;
+        console.log(value, name);
+        if (expresionRegular.test(this.state.email)) {
             console.log("valido");
-            
-            axios.get('http://localhost:3001/personas')
+
+              axios.post('http://10.0.0.68:81/personas/', {
+                nombre: this.state.nombre,
+                apellido: this.state.apellido,
+                tipoDocumento: this.state.tipo,
+                documento: this.state.documento,
+                email: this.state.email
+              })
               .then(function (response) {
-                console.log(response);
+                console.log("Esto se envia a la base de datos");
+                console.log(response.data.data);
               })
               .catch(function (error) {
                 console.log(error);
-              });
+              });    
         }
         else
             console.log("Invalido");
-    }   
 
-    validarString = (e) =>{
-        const {value, name} = e.target;
-        var expresionRegular=/^\w{0,5}$/;
-        if(expresionRegular.test(value)){
-        console.log(value,name);
-        // console.log("Error");
+            
+    }
+
+    validarString = (e) => {
+        const { value, name } = e.target;
+        var expresionRegular = /^\w{0,5}$/;
+        if (expresionRegular.test(value)) {
+            console.log(value, name);
+            // console.log("Error");
             this.setState({
                 [name]: value
-              });
+            });
         }
-       
+
     }
-    
-    render(){ {console.log("dentro de main, antes de mandar DB")}
-                 {console.log(this.state.datos)}
-        return(
-               
-        
+
+    handleTipo = (e) => {
+        const { value, name } = e.target;
+        let valor;
+        (value == "DNI") ? valor = 1 : (value == "Cedula") ? valor = 2: valor=3; 
+        console.log("valor de Tipo documento");
+        console.log(valor);
+        this.setState({
+            tipo: valor
+        });
+
+    }
+
+
+
+    render() {
+        { console.log("dentro de main, antes de mandar DB") }
+        { console.log(this.state.datos) }
+        return (
+
+
             <div className="container">
-                <Tablas db={this.state.datos}/>
+                <Tablas db={this.state.datos} onChange={this.actualizar}/>
 
                 <div className="row mt-4">
                     <div className="card">
                         <form onSubmit={this.handleSubmit}
-                        // onSubmit={} 
-                         className="card-body">
+                            // onSubmit={} 
+                            className="card-body">
                             <div className="form-group">
-                            <input
-                                type="text"
-                                name="nombre"
-                                className="form-control"
-                                value={this.state.nombre}
-                                onChange={this.validarString}
-                                placeholder="Nombre"
+                                <input
+                                    type="text"
+                                    name="nombre"
+                                    className="form-control"
+                                    value={this.state.nombre}
+                                    onChange={this.validarString}
+                                    placeholder="Nombre"
                                 />
                             </div>
                             <div className="form-group">
@@ -131,14 +157,14 @@ class Main extends Component{
                                     value={this.state.apellido}
                                     onChange={this.validarString}
                                     placeholder="Apellido"
-                                    />
+                                />
                             </div>
                             <div className="form-group">
                                 <select
                                     name="tipo"
                                     className="form-control"
-                                    //   value={}
-                                    //   onChange={}
+                                    // value={}
+                                    onChange={ this.handleTipo}
                                 >
                                     <option>DNI</option>
                                     <option>Cedula</option>
@@ -153,7 +179,7 @@ class Main extends Component{
                                     value={this.state.documento}
                                     onChange={this.validarNumero}
                                     placeholder="Documento"
-                                    />
+                                />
                             </div>
                             <div className="form-group">
                                 <input
@@ -163,17 +189,18 @@ class Main extends Component{
                                     value={this.state.email}
                                     onChange={this.validarMail}
                                     placeholder="Email"
-                                    />
+                                />
                             </div>
-                            
 
-                            <button type="submit" className="btn btn-primary">
-                            Save
+
+                            <button type="submit" className="btn btn-primary" onClick={this.actualizar}>
+                                Save
                             </button>
                         </form>
                     </div>
                 </div>
-            </div> 
+
+            </div>
         );
     }
 }
--- prueba/src/Componentes/Tablas.js
+++ prueba/src/Componentes/Tablas.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { Table} from 'reactstrap';
-
+import { Table ,Button} from 'reactstrap';
+import axios from 'axios';
 
 
 class Tablas extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         
     }
 
+    eliminar  = (e) =>{
 
+
+        console.log("This:",this)
+   
+        axios.delete('http://10.0.0.68:81/personas/'+e)
+            .then( (response)=> {
+                // handle success
+                console.log(response);
+
+                console.log("Entro al eliminar");
+                console.log(this.props.onChange);
+                
+                if(this.props.onChange)
+                    this.props.onChange();
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
     
     render(){
         return(
@@ -25,17 +48,18 @@ class Tablas extends Component{
                         <th>Email</th>
                     </tr>
                 </thead>
-                <tbody>
+                <tbody >
                     {console.log("dentro de la tabla")}
                     {console.log(this.props.db)}
-                    {   this.props.db.map(persona => 
-                         <tr>
-                            <th scope="row">1</th>
+                    {   this.props.db.map((persona, i) => 
+                         <tr key={i}>
+                            <th scope="row">{persona.id}</th>
                             <td>{persona.nombre}</td>
                             <td>{persona.apellido}</td>
-                            <td>{persona.Documento}</td>
-                            <td>dd</td>
-                            <td>dd</td>
+                            <td>{persona.tipoDocumento}</td>
+                            <td>{persona.documento}</td>
+                            <td>{persona.email}</td>
+                            <td> <Button color="danger" onClick={()=>this.eliminar(persona.id)}>Eliminar</Button> </td>
                          </tr>
                         )}
                    
